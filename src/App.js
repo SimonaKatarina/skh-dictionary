c96edbe536cc77205a4df0f54ec16f1671f5979a
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-30 21:32:00 +0200

    Initial commit for Dictionary app
    First edits made

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import skhlogo from './skhlogo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <img src={skhlogo} className='App-logo img-fluid' alt='logo'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
