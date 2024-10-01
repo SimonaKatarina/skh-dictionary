d72afd1c539ae16331bfb4b86307b07b235cdd0b
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-10-01 17:13:44 +0200

    Axios added
    Intergraded API dictionary and installed axios.
    
    API documentation included.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,23 @@
 import React, {useState} from "react";
+import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary(){
 let [keyword, setKeyword]=useState(null);
 
+function handleResponse(response){
+    console.log(response.data[0]);
+}
 
  function search(event){
     event.preventDefault();
     alert(`${keyword}`);
- }
+
+    // documentation: https://api.dictionaryapi.dev/
+    let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+}
+
 
  function handleKeywordChange(event){
  
